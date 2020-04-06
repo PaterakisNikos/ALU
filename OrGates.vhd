f3dbf38532e90aba3ef67f5379b132ca89789dfa
PaterakisNikos <48360871+PaterakisNikos@users.noreply.github.com>
2020-04-06 16:00:14 +0300

    Add files via upload

--- OrGates.vhd
+++ OrGates.vhd
@@ -21,6 +21,27 @@ END Struct;
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 
+-- OR Gate for 3 Inputs--
+
+ENTITY OrGate3 IS
+PORT
+(
+	input0: IN std_logic;
+	input1: IN std_logic;
+	input2: IN std_logic;
+	output: OUT std_logic
+);
+END OrGate3;
+
+ARCHITECTURE Struct OF OrGate3 IS
+BEGIN
+	output <= input0 OR input1 OR input2;
+END Struct;
+
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+
 -- OR Gate for 4 Inputs--
 
 ENTITY OrGate4 IS
@@ -32,7 +53,7 @@ PORT
 	input3: IN std_logic;
 	output: OUT std_logic
 );
-END OrGate3;
+END OrGate4;
 
 ARCHITECTURE Struct OF OrGate4 IS
 BEGIN
